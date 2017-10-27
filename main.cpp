bfb78d6f0e13c92887b07e42009605fd0310540b
erwanlecarpentier <erwan.lc2@gmail.com>
2017-10-27 16:13:37 +0200

    Main structure changed

--- main.cpp
+++ main.cpp
@@ -13,13 +13,29 @@
 #include <parameters.hpp>
 #include <environment.hpp>
 #include <agent.hpp>
+#include <random_policy.hpp>
 
-void run() {
-    parameters p("config/main.cfg"); // read parameters
-    environment e(p); // create environment
-    agent a(p); // create agent
-    printv(a.state);
-    e.print_grid();
+template <class PLC>
+void run(bool print, const parameters &p) {
+    environment e(p);
+    agent<PLC> a(p);
+    if(print) {
+        e.print_grid();
+        printv(a.state);
+    }
+    while(!e.is_terminal(a.state)) {
+        //TODO
+        //a.apply_policy();
+    }
+}
+
+void run_switch(const parameters &p)
+{
+    bool print = true;
+    switch(p.POLICY_SELECTOR)
+    {
+        case 0: run< random_policy >(print, p); break;
+    }
 }
 
 /**
@@ -28,7 +44,7 @@ void run() {
 int main() {
     try {
         srand(time(NULL));
-        run();
+        run_switch(parameters("config/main.cfg"));
     }
     catch(const std::exception &e) {
         std::cerr<<"Error in main(): standard exception caught: "<<e.what()<<std::endl;
