d2510b7820b3b994df92c727689958cce30fc2d6
erwanlecarpentier <erwan.lc2@gmail.com>
2017-10-27 14:45:29 +0200

    Environment updated

--- src/environment.hpp
+++ src/environment.hpp
@@ -20,6 +20,39 @@ struct environment {
     }
 
     /**
+     * @brief Grid world value
+     *
+     * Evaluate the grid world at the given state.
+     * @param {const std::vector<double> &} s; given state
+     * @return Return the value of the grid.
+     */
+    int grid_world_value(const std::vector<double> &s) {
+        assert(s.size() == 2);
+        return grid_world.at(s.at(0)).at(s.at(1));
+    }
+
+    /**
+     * @brief Is terminal
+     *
+     * Test if the given state is terminal.
+     * Convention for gridworld if 1 for a terminal grid cell.
+     * @param {const std::vector<double> &} s; given state
+     * @return Return true if the test is terminal, else false.
+     */
+    bool is_terminal(const std::vector<double> &s) {
+        if(is_continuous) {
+            std::cout << "TODO: implement continuous world" << std::endl;
+            return true;
+        } else {
+            if(grid_world_value(s) == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
      * @brief Print grid
      *
      * Print the grid world as a standard output.
