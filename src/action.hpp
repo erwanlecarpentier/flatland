5e025c0c9e36de1aa001e40f9e9215d86a119245
erwanlecarpentier <erwan.lc2@gmail.com>
2017-11-07 14:08:48 +0100

    Velocity and orientation added to state and cartesian actions

--- src/action.hpp
+++ src/action.hpp
@@ -1,18 +1,43 @@
 #ifndef ACTION_HPP_
 #define ACTION_HPP_
 
+#include <state.hpp>
+
 /**
  * @brief Action class
  */
 class action {
 public:
-    double dx;
-    double dy;
+    double dx; ///< variation along x coordinate
+    double dy; ///< variation along y coordinate
+    double _v; ///< absolute velocity
+    double _theta; ///< angle with x axis (rad)
 
     /**
      * @brief Default constructor
      */
-    action(double _dx = 0., double _dy = 0.) : dx(_dx), dy(_dy) { /* Nothing to do */ }
+    action(
+        double _dx = 0.,
+        double _dy = 0.) :
+        dx(_dx),
+        dy(_dy)
+    {
+        _v = sqrt(dx * dx + dy * dy);
+        _theta = atan(dy / dy);
+    }
+
+    /**
+     * @brief Apply action
+     *
+     * Modifiy the input state by applying the action.
+     * @param {state &} s; modified state
+     */
+    void apply(state &s) {
+        s.x += dx;
+        s.y += dy;
+        s.v = _v;
+        s.theta = _theta;
+    }
 
     /**
      * @brief Set to default
--- src/state.hpp
+++ src/state.hpp
@@ -6,13 +6,24 @@
  */
 class state {
 public:
-    double x;
-    double y;
+    double x; ///< x coordinate
+    double y; ///< y coordinate
+    double v; ///< velocity
+    double theta; ///< angle with x axis (rad)
 
     /**
      * @brief Default constructor
      */
-    state(double _x = 0., double _y = 0.) : x(_x), y(_y) { /* Nothing to do */ }
+    state(
+        double _x = 0.,
+        double _y = 0.,
+        double _v = 1.,
+        double _theta = 0.) :
+        x(_x),
+        y(_y),
+        v(_v),
+        theta(_theta)
+    { /* Nothing to do */ }
 
     /**
      * @brief Set to default
@@ -22,6 +33,8 @@ public:
     void set_to_default() {
         x = 0.;
         y = 0.;
+        v = 1.;
+        theta = 0.;
     }
 };
 
